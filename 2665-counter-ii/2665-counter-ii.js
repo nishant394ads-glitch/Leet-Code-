2dc37df3ac2786010d344b1dcf8855554411c5b8
Nishant S <nishant394ads@gmail.com>
2026-01-07 19:15:57 +0530

    Time: 44 ms (88.56%), Space: 56.7 MB (40.42%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} init
+ * @return {{ increment: Function, decrement: Function, reset: Function }}
+ */
+var createCounter = function(init) {
+    let current = init;
+
+    return {
+        increment: function() {
+            return ++current;
+        },
+        decrement: function() {
+            return --current;
+        },
+        reset: function() {
+            current = init;
+            return current;
+        }
+    };
+};
