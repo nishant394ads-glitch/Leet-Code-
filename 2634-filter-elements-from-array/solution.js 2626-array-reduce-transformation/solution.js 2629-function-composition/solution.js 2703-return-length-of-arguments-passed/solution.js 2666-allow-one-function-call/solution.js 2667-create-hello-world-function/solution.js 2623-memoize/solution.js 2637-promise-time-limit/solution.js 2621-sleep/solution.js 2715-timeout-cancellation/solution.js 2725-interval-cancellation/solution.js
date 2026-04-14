1a863a8d1ac5646a66995431eb9f41f7f4cdc47d
Nishant S <nishant394ads@gmail.com>
2026-04-14 13:16:18 +0530

    Add cancellable function to execute repeatedly

--- 2634-filter-elements-from-array/solution.js 2626-array-reduce-transformation/solution.js 2629-function-composition/solution.js 2703-return-length-of-arguments-passed/solution.js 2666-allow-one-function-call/solution.js 2667-create-hello-world-function/solution.js 2623-memoize/solution.js 2637-promise-time-limit/solution.js 2621-sleep/solution.js 2715-timeout-cancellation/solution.js 2725-interval-cancellation/solution.js
+++ 2634-filter-elements-from-array/solution.js 2626-array-reduce-transformation/solution.js 2629-function-composition/solution.js 2703-return-length-of-arguments-passed/solution.js 2666-allow-one-function-call/solution.js 2667-create-hello-world-function/solution.js 2623-memoize/solution.js 2637-promise-time-limit/solution.js 2621-sleep/solution.js 2715-timeout-cancellation/solution.js 2725-interval-cancellation/solution.js
@@ -1,1 +1,11 @@
-
+var cancellable = function(fn, args, t) {
+    fn(...args); // run immediately
+    
+    const intervalId = setInterval(() => {
+        fn(...args);
+    }, t);
+    
+    return function cancelFn() {
+        clearInterval(intervalId);
+    };
+};
