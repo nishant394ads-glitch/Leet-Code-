011cd09e78e31ac419cf1dadfd6162701bbd0b2c
Nishant S <nishant394ads@gmail.com>
2026-08-05 19:24:55 +0530

    Time: 195 ms (99.24%), Space: 99.6 MB (89.39%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Solution:
+    def remainingMethods(self, n, k, invocations):
+        graph = [[] for _ in range(n)]
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        # Find all suspicious methods
+        suspicious = [False] * n
+        q = deque([k])
+        suspicious[k] = True
+
+        while q:
+            u = q.popleft()
+            for v in graph[u]:
+                if not suspicious[v]:
+                    suspicious[v] = True
+                    q.append(v)
+
+        # Check if any outside method invokes a suspicious method
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        # Return remaining methods
+        ans = []
+        for i in range(n):
+            if not suspicious[i]:
+                ans.append(i)
+
+        return ans
